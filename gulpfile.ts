86b1a63481673a5b6cf08b3c69a075c841224444
Lumie <lummybird@gmail.com>
2024-03-30 20:32:36 +0000

    chore: add boilerplate and build

---
+++ gulpfile.ts
@@ -0,0 +1,87 @@
+import gulp from 'gulp';
+import watch from 'gulp-watch';
+import sass from 'sass';
+import sassGulp from 'gulp-sass';
+import uglify from 'gulp-uglify';
+import browserify from 'browserify';
+import tsify from 'tsify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import concat from 'gulp-concat';
+import uglifycss from 'gulp-uglifycss';
+
+const Sassify = sassGulp(sass);
+const buildRoot = 'www';
+
+const buildAndBundleSite = (isMinified: boolean) => {
+    const chain = browserify({
+        entries: ["src/index.ts"],
+        debug: true,
+      })
+      .plugin(tsify, {
+        project: 'tsconfig.json',
+      })  
+      .transform(babelify.configure({
+        presets: ['@babel/preset-typescript'],
+      }))
+      .bundle()
+      .pipe(source('bundle.js'))
+      .pipe(buffer());
+
+      return (isMinified ? chain.pipe(uglify()) : chain).pipe(gulp.dest(buildRoot))
+}
+
+const buildAndBundleStyle = (isMinified: boolean) => {
+    const chain = gulp.src('src/**/*.scss')
+      .pipe(Sassify())
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest(buildRoot));
+
+    // Is minifed attach pipes or just skip the uglifiy
+    return (isMinified ? chain.pipe(uglifycss({"maxLineLen": 80, "uglyComments": true })) : chain).pipe(gulp.dest(buildRoot))
+}
+
+gulp.task("build-and-bundle-site", () => buildAndBundleSite(false));
+gulp.task('build-and-bundle-styles', () => buildAndBundleStyle(false));
+gulp.task("build-minified-and-bundle-site", () => buildAndBundleSite(true));
+gulp.task('build-minified-and-bundle-styles', () => buildAndBundleStyle(true));
+
+// Watching tasks
+gulp.task('build-and-bundle-site-watch', () => gulp.watch('src/**/*.ts', gulp.series('build-and-bundle-site')));
+gulp.task('build-and-bundle-styles-watch', () => gulp.watch('src/**/*.scss', gulp.series('build-and-bundle-styles')));
+gulp.task('copy-statics-watch', () => gulp.watch('src/**/*.html', gulp.series('copy-statics')));
+
+gulp.task("copy-statics", (done) => {
+    return gulp
+    .src('src/static/**/*')
+    .pipe(gulp.dest(buildRoot));
+});
+
+gulp.task("copy-assets", (done) => {
+  return gulp
+  .src('src/assets/**/*')
+  .pipe(gulp.dest(buildRoot+'/assets'));
+});
+
+gulp.task("build-test", gulp.parallel([
+  'build-and-bundle-site',
+  'build-and-bundle-styles', 
+  'copy-statics',
+  'copy-assets'
+]));
+
+
+gulp.task("build-production", gulp.parallel([
+  'build-minified-and-bundle-site',
+  'build-minified-and-bundle-styles', 
+  'copy-statics',
+  'copy-assets'
+]));
+
+gulp.task("watch", gulp.parallel([
+  'build-production',
+  'build-and-bundle-site-watch',
+  'build-and-bundle-styles-watch', 
+  'copy-statics-watch',
+]));
---
+++ src/assets/README.md
@@ -0,0 +1,2 @@
+# Hello!
+This is where all the assets go for the website, such as images, fonts and all that good stuff.
