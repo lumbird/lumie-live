6b79c2b73774c2de80b23c955a6d155d7aa1608d
Lumie <lummybird@gmail.com>
2024-04-18 17:44:23 +0100

    chore: add routing logic and interfaces

--- src/stores/app/app.interfaces.ts
+++ src/stores/app/app.interfaces.ts
@@ -1,7 +1,9 @@
 import { ButtonState } from "../buttons/buttons.interface";
 import { FeedState } from '../feed/feed.interface';
+import { RouteState } from '../route/route.interface';
 
 export interface AppState {
     buttonState: ButtonState;
     feedState: FeedState;
+    routeState: RouteState;
 }
--- src/stores/route/route.reducers.ts
+++ src/stores/route/route.reducers.ts
@@ -1,6 +1,51 @@
 import { AppState } from '../app/app.interfaces';
 import { Action } from '@lumieslab/stasho/dist/interfaces/store-interfaces';
+import { filteredReducer } from '@lumieslab/stasho';
+import { goBackAction, gotoLocationRouteAction, gotoLocationRoutePathAction } from './route.action';
 
-export function gotoRoute(state: AppState, action: Action): AppState {
+const gotoRoutePathReducer = filteredReducer([
+    gotoLocationRoutePathAction
+], (state: AppState, action: ReturnType<typeof gotoLocationRoutePathAction>) => {
+    const route = action.routePath.split(state.routeState.delimiter);
+
+    state.routeState = {
+        ... state.routeState,
+        route: route,
+        routePath: action.routePath
+    }
+
     return state;
-}
+});
+
+const gotoRouteReducer = filteredReducer([
+    gotoLocationRouteAction
+], (state: AppState, action: ReturnType<typeof gotoLocationRouteAction>) => {
+    const routePath = action.route.join(state.routeState.delimiter);
+
+    state.routeState = {
+        ... state.routeState,
+        route: action.route,
+        routePath: routePath
+    }
+
+    return state;
+});
+
+const goBackReducer = filteredReducer([
+    goBackAction
+], (state: AppState, action: Action) => {
+
+    if(state.routeState.route.length > 0) {
+        state.routeState.route.pop();
+
+        state.routeState = {
+            ... state.routeState,
+            route: state.routeState.route,
+            routePath: state.routeState.route.join(state.routeState.delimiter)
+        }
+    }
+
+    return state;
+});
+
+export const routeReducers = [gotoRoutePathReducer, gotoRouteReducer, goBackReducer];
