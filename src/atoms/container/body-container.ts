2e174eef5722dce57eaaab665edd9283c025e7d5
Lumie <lummybird@gmail.com>
2024-05-03 10:19:10 +0100

    chore: add body content routes into body container

--- src/atoms/container/body-container.ts
+++ src/atoms/container/body-container.ts
@@ -2,8 +2,13 @@ import { div } from '@lumieslab/skribl';
 import { getStore } from '../../stores/app/app.store';
 import { routeMemory } from '../../stores/route/route.memories';
 
-export function contentFromRoute(route: string) {
+type PageRoutes = 'feed' | 'gallery' | 'projects';
+
+export function contentFromRoute(route: PageRoutes) {
     switch (route) {
+        case "feed":
+        case "gallery":
+        case "projects":
         default:
             return div({class: 'item'},);
     }
@@ -14,6 +19,6 @@ export function bodyContainer(): HTMLElement {
     const store = getStore();
     const routes = store.getMemory(routeMemory);
     return div({class: 'body-container full-width'},
-        contentFromRoute(routes[0])
+        contentFromRoute(routes[0] as PageRoutes)
     );
 }
