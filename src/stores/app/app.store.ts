12aa0c8a0fb6c612d633c86e1340e520eaef3dec
Lumie <lummybird@gmail.com>
2024-05-05 11:04:44 +0100

    chore: add feed memories

--- src/stores/app/app.store.ts
+++ src/stores/app/app.store.ts
@@ -1,6 +1,5 @@
 import { Store } from '@lumieslab/stasho';
 import { AppState } from './app.interfaces';
-import { CombinedContext } from '@lumieslab/stasho/dist/interfaces/context-typing';
 import { appEffects } from './app.effects';
 import { buttonState } from '../buttons/buttons.store';
 import { buttonMemories } from '../buttons/buttons. memories';
@@ -9,7 +8,7 @@ import { feedState } from '../feed/feed.store';
 import { routeState } from '../route/route.store';
 import { routeReducers } from '../route/route.reducers';
 import { routeMemories } from '../route/route.memories';
-
+import { feedMemories } from '../feed/feed.memories';
 
 const state: AppState = {
     buttonState,
@@ -23,7 +22,8 @@ export function getStore() {
         routeReducers
     ], [
         ...buttonMemories,
-        ...routeMemories
+        ...routeMemories,
+        ...feedMemories
     ], [
         ...appEffects
     ]);
--- src/stores/feed/feed.memories.ts
+++ src/stores/feed/feed.memories.ts
@@ -3,3 +3,5 @@ import { AppState } from '../app/app.interfaces';
 export function postsMemory(appState: AppState) {
     return appState.feedState.posts;
 }
+
+export const feedMemories = [postsMemory];
